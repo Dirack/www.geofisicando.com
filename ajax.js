cc597ed047dcd0a70f09e6b62ffddebdf2f66a94
Dirack <rodolfo_profissional@hotmail.com>
2024-02-12 14:41:45 -0300

    [WIP] Versão inicial do site geofisicando
    Esta é a última versão do site extraída do repositório antigo. O site é feito em PHP com banco de dados MySQL e as postagens são carregadas utilizando o ajax. Esta versão deve ser modificada e atualizada para uma versão mínima a ser publicada no site oficial

---
+++ ajax.js
@@ -0,0 +1,95 @@
+/*
+	ajax.js (JavaScript)
+	
+	Objetivo: Exibir os posts recentes na página index.html
+	a partir dos dados na página posts.xml feito por solicitação AJAX. 
+	Adaptado do estudo sobre solicitação Ajax em JavaScript.
+	
+	Email: rodolfo_profissional@hotmail.com
+
+	Original: Rodolfo A. C. Neves 16/01/2019
+
+	Programador: Rodolfo A. C. Neves 20/01/2019
+*/
+
+
+
+function ajax(){
+
+	/*
+		XMLHttpRequest() é um objeto que permite fazer a comunicação com o
+		servidor de forma dinâmica.
+	*/
+	var xmlHttp = new XMLHttpRequest();
+
+	/* 
+		onreadystatechange significa que a função abaixo será chamada toda
+		vez que a propriedade readystate for alterada. readyState detém o
+		status da XMLHttpRequest. Pode assumir os seguintes valores:
+			0: requisição não inicializada
+			1: Conexão com o servidor estabelecida
+			2: requisição recebida
+			3: processando requisição
+			4: requisição terminada e a resposta está pronta 
+		A propriedade status retorna o status-number de uma requisição:
+			200: "OK"
+			403: "Forbidden"
+			404: "Not Found"
+		Ou seja, o if na função abaixo permite que a resposta seja retornada se
+		a requisição está terminada e pronta (readyState=4) e tudo ocorreu bem
+		com a requisição (status=200).
+		A propriedade responseText o servidor responde com texto.
+		Se utilizar responseXML o servidor responde com um arquivo XML.
+	*/
+
+	
+	/* 
+		A saída foi adaptada para o formato exigido pela págiga index.html.
+		A função abaixo pode ser readaptada para exibir qualquer formato
+		html exigido pela página.
+
+	*/
+	xmlHttp.onreadystatechange = function(){	
+		if(xmlHttp.readyState===4&&xmlHttp.status===200){
+		
+			xmlDoc = xmlHttp.responseXML;
+			txt = "";
+			
+			autor = xmlDoc.getElementsByTagName("autor");
+			data = xmlDoc.getElementsByTagName("data");
+			fonte = xmlDoc.getElementsByTagName("fonte");
+			thumb = xmlDoc.getElementsByTagName("thumb");
+			titulo = xmlDoc.getElementsByTagName("titulo");
+			resumo = xmlDoc.getElementsByTagName("resumo");
+			foto = xmlDoc.getElementsByTagName("foto");
+
+
+			for(i=0; i<autor.length; i++){
+			    			
+			    // Formatar data de AAAA-MM-DD para DD/MM/AAAA
+			    dataform = data[i].childNodes[0].nodeValue.split('-');
+			
+				//txt += '<a href='+fonte[i].childNodes[0].nodeValue+' title="Ler artigo completo?">';
+				txt += '<h1><a href='+fonte[i].childNodes[0].nodeValue+'#column-center title="Ler artigo completo?" style="border-bottom: none;">';
+				txt += titulo[i].childNodes[0].nodeValue+'</a></h1>';
+				txt += "<p><small>Por <em>"+autor[i].childNodes[0].nodeValue + "</em> em ";
+				// Data formatada para DD/MM/AAAA
+				txt +=  +dataform[2]+"/"+dataform[1]+"/"+dataform[0]+ "</small></p>";
+				txt += '<img src="'+foto[i].childNodes[0].nodeValue+'" width="700px" height="500px">';	
+				txt += '<p id="descricao">'+resumo[i].childNodes[0].nodeValue + "</p><br>";
+				txt += '<a href="'+fonte[i].childNodes[0].nodeValue+'#column-center" style="border-bottom: none;" title="Ler artigo completo?">Leia mais...</a><br><br>';
+			}
+			
+
+			document.getElementById("postagens").innerHTML = txt;
+		}
+	};
+
+	/* Abre o arquivo ajax.txt com o método GET*/
+	xmlHttp.open("GET","posts.xml",true);
+
+	/* Envia para a página */
+	xmlHttp.send(null);
+
+	
+}
