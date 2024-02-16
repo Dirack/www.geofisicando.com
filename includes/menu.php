64a903436bb6de76ed2ad7f028431bbd9db4b29f
Dirack <rodolfo_profissional@hotmail.com>
2024-02-16 16:15:45 -0300

    [WIP] build navbar using a new menu class

--- includes/menu.php
+++ includes/menu.php
@@ -3,18 +3,23 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
 error_reporting(E_ALL);
-?>
 
-	<!-- Menu de Navegação principal do site-->
-	<!-- Classe selected são os botões do menu de navegação -->
+include "includes/Menu.php";
+?>
 	<nav id="menu">
 		<div id="menu-center">
 			<ul id="homebotao">
-				<?php foreach($MENU_ARRAY as $chave => $valor){
-				echo '<li class="selected">';
-				echo '<a href="'.$valor.'">'.$chave.'</a>';
-				echo '</li>';
-				}?>
+				<?php
+					$menu = new Menu();
+					$menu_array=array(
+						"HOME"=>"artigos/index.php",
+						"TUTORIAIS"=>"artigos/index.php",
+						"CURSOS"=>"artigos/index.php",
+						"PROGRAMAS"=>"artigos/index.php",
+						"JOGOS"=>"artigos/index.php"
+					);
+					echo $menu->buildMenuFromArray($menu_array);
+				?>
 			</ul>
 		</div>
 	</nav>
